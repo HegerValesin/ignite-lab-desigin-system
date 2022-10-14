c4e813a8d5f20ef5dd4c04c1d7b3c4a3130da988
Heger Valesin <hegervfp@gmail.com>
2022-10-14 13:26:35 -0300

    Criando desigin system

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-violet-500 mb-3">projeto ignite</h1>
+
+      <button className='text-white bg-cyan-300 font-medium px-4 py-2 rounded hover:bg-cyan-500'>Enviar</button>
+    </>
+  )
+}
